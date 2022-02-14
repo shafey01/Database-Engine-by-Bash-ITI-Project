1388e9c99530445fc48a9561fc9818f741e7e106
IslamKortam <eng.islam.kortam@gmail.com>
2022-02-14 21:47:40 +0200

    Solving a bug in showDatabases.sh

--- useDB.sh
+++ useDB.sh
@@ -2,7 +2,7 @@
 
 #useDatabaseMenu
 useDatabaseMainMenu(){
-    ehco "Selected Database is: $current_DB";
+    echo "Selected Database is: $current_DB";
     options=("Show Tables", "Create New Table", "Delete existing Table", "Insert into Table", "Update Table", "Select from Table", "Return to main menu");
     while [[ "$option" != "Return to main menu" ]] 
     do
@@ -12,10 +12,10 @@ useDatabaseMainMenu(){
             "Show Tables") . ./showTables.sh;break ;;
             "Create New Table") . ./createTable.sh; break;;
             "Insert into Table") . ./insertIntoTable.sh; break;;
-            "Delete existing Table") . ./deleteTable.sh; break ;;
+            "Drop existing Table") . ./deleteTable.sh; break ;;
             "Update Table") . ./updateTable.sh; break ;;
             "Select from Table") . ./selectFromTable.sh; break;;
-            "Return to main menu") . ./mainMenu.sh; exit $? ;;
+            "Return to main menu") . ./main.sh; exit $? ;;
             *) echo "Invalid option $REPLY , choose again";;
         esac
     done
@@ -36,6 +36,7 @@ then
     #Database exists
     echo "Database $current_DB is selected";
     export current_DB;
+	useDatabaseMainMenu;
 else
     #Database deosn't exist
     echo "Couldn't find database with name: $current_DB";

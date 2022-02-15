5f254054ffc491972f58b137bcee553bbcaf5344
IslamKortam <eng.islam.kortam@gmail.com>
2022-02-15 19:13:56 +0200

    Implementing Delete Record

---
+++ deleteRecord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dataFilePath="./Databases/${current_DB}/Data/${current_table}";
+metadataFilePath="./Databases/${current_DB}/MetaData/${current_table}.metadata";
+
+echo "enter column name: ";
+read col;
+
+ret=$( cut -d: -f1 ${metadataFilePath} | grep ${col}; );
+if [[ ${ret} == ${col} ]] ; then
+    #column found
+    colNum=$(awk -F: -v c=$col '{if(c == $1){print NR}}' $metadataFilePath);
+    echo "Enter value to be deleted: ";
+    read val;
+    awk -i inplace -F, -v colNumber=$colNum -v value=$val '{if($colNumber != value){print $0}}' ${dataFilePath};
+else
+    echo 'No such column';
+fi
